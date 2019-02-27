54e6f4e02ebe96e3f6b9bfe0746f33d413504071
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-02-27 12:07:52 +0100

    A few codes that might be useful

--- Main/Surface Model/sol_to_BC.py
+++ Main/Surface Model/sol_to_BC.py
@@ -6,15 +6,17 @@ import sys
 import os
 import argparse
 
-numstep = 100
+numstep = 50
 Time = 100
 dt = Time/numstep
 k = 0.0
 t = Constant(k)
 mesh = UnitIntervalMesh(5)
 mesh = UnitSquareMesh(15, 15)
-mesh = UnitCubeMesh(15, 15, 15)
+#mesh = UnitCubeMesh(15, 30, 30)
+n0 = FacetNormal(mesh)
 
+print((mesh.num_cells()))
 class Left(SubDomain):
     def inside(self, x, on_boundary):
         return near(x[0], 0.0)
@@ -23,6 +25,7 @@ class Left(SubDomain):
 class Right(SubDomain):
     def inside(self, x, on_boundary):
         return near(x[0], 1.0)
+
 
 class Bottom(SubDomain):
     def inside(self, x, on_boundary):
@@ -38,31 +41,88 @@ V = FunctionSpace(mesh, "CG", 1)
 boundarymesh = BoundaryMesh(mesh, 'exterior')
 submesh = SubMesh(boundarymesh, Left())
 parameters['allow_extrapolation'] = True
-intmesh = UnitIntervalMesh(20)
-W = FunctionSpace(submesh, "CG", 1)
-u = TrialFunction(V)
-u2 = TrialFunction(W)
+
+
+W = VectorFunctionSpace(submesh, "CG", 1, dim=2)
+
+c_m = Function(V)
 v = TestFunction(V)
-v2 = TestFunction(W)
-u_n = Function(V)
-u2_n = Function(W)
+c_m_n = Function(V)
 
-F = 0.01*dot(grad(u), grad(v))*dx
+F = (c_m - c_m_n)/dt*v*dx+ 0.1*dot(grad(c_m), grad(v))*dx
 
-F2 = 0.001*dot(grad(u2), grad(v2))*dx # - Expression("2+sin(x[0]-0.5)", degree=2)*v2*dx
-bc2 = [DirichletBC(W, 1.0, Bottom()), DirichletBC(W, 0.0, Top())]
+u = Function(W)
+cm0, c_surf = split(u)
 
-u = Function(V)
-u2 = Function(W)
+v0, v_surf = TestFunctions(W)
+u_n = Function(W)
+cm0_n, c_surf_n = split(u_n)
+
+
+temp = 300
+rho = 6.3e28
+n_surf = 6.9*rho**(2/3)
+print(n_surf)
+n_TIS = 6*rho
+Pr = 0.81
+phi_atom = Expression('5.8e19*1/(width*pow(2*3.14,0.5))*  \
+               exp(-0.5*(pow(((x[1]-center)/width), 2)+pow(((x[2]-center)/width), 2)))',
+               degree=2, center=0.5, width=0.15) #5.8e19
+sigma_exc = 1.7e-21
+k_B = 8.6e-5  # Boltzmann constant
+nu_0d = 1e13
+nu_0sb = 1e13
+
+
+nu_0bs = 1e13
+lambda_des = 1/(n_surf**(1/2))
+lambda_abs = n_surf/n_TIS
+E_D = 0.69
+E_A = 1.33
+E_R = 0.2
+
+n02 = FacetNormal(submesh)
+print(n0)
+A = dot(grad(c_m_n), n02)
+A = grad(c_m_n)
+a = interpolate(A, FunctionSpace(mesh, "DG", 0))
+
+gradient = project(dot(grad(c_m_n),n02), FunctionSpace(boundarymesh, "CG", 1)) #dot(grad(c_m_n), n0)
+print(gradient(0, 0.5))
+
+alpha = 110e-12
+D = 1e-17
+teta = c_surf / n_surf
+phi_atom = (1 - Pr)*phi_atom*(1 - teta)
+phi_exc = phi_atom*sigma_exc*c_surf
+phi_desorb = 2*nu_0d*lambda_des**2*np.exp(-2*E_D/k_B/temp)*c_surf**2
+phi_sb = nu_0sb*np.exp(-E_A/k_B/temp)*c_surf
+phi_bs = nu_0bs*lambda_abs*np.exp(-E_R/k_B/temp)*cm0*(1-teta)
+phi_diff = -D*gradient
+
+F2 = (cm0 - cm0_n)/dt*v0*dx + (c_surf - c_surf_n)/dt*v_surf*dx
+
+F2 += -phi_atom*v_surf*dx + phi_exc*v_surf*dx + phi_desorb*v_surf*dx + phi_sb*v_surf*dx - phi_bs*v_surf*dx
+
+F2 += 1/alpha * (-phi_sb * v0*dx + phi_bs*v0*dx + phi_diff*v0*dx)
+
+
+set_log_level(30)
+
 fileu = File("Solution/u.pvd")
-fileu2 = File("Solution/u2.pvd")
+fileu2 = File("Solution/c_m.pvd")
 
-#parameters["allow_extrapolation"] = True
-parameters['allow_extrapolation'] = False
-solve(lhs(F2) == rhs(F2), u2, bc2)
-bcs = [DirichletBC(V, u2, Left()), DirichletBC(V, 1.5, Right())]
-
-solve(lhs(F) == rhs(F), u, bcs)
-
-fileu << u
-fileu2 << u2
+for i in range(numstep):
+    print(str(round(k/Time*100, 2)) + ' %        ' + str(round(t, 1)) + ' s',
+      end="\r")
+    solve(F2 == 0, u, [])
+    cm0, c_surf = u.split()
+    bcs = [DirichletBC(V, cm0, Left())]
+    #parameters['allow_extrapolation'] = False
+    u_n.assign(u)
+    solve(F == 0, c_m, bcs)
+    c_m_n.assign(c_m)
+    k += dt
+    t.assign(k)
+    fileu << (u, k)
+    fileu2 << (c_m, k)
--- Main/Surface Model/surface_model_exemple.py
+++ Main/Surface Model/surface_model_exemple.py
@@ -5,33 +5,70 @@ import csv
 import sys
 import os
 import argparse
+import scipy
 
-numstep = 10000
-Time = 100
-dt = Time/numstep
-k = 0.0
-t = Constant(k)
+def adaptative_timestep(converged, nb_it, dt, dt_min, dt_max,
+                        stepsize_change_ratio, t, t_stop,
+                        stepsize_stop_max):
+    '''
+    Adapts the stepsize as function of the number of iterations of the
+    solver.
+    Arguments:
+    - converged : bool, determines if the time step has converged.
+    - nb_it : int, number of iterations
+    - dt : Constant(), fenics object
+    - dt_min : float, stepsize minimum value
+    - stepsize_change_ration : float, stepsize change ratio
+    - t : float, time
+    - t_stop : float, time where adaptative time step stops
+    - stepsize_stop_max : float, maximum stepsize after stop
+    Returns:
+    - dt : Constant(), fenics object
+    '''
+    while converged is False:
+        dt.assign(float(dt)/stepsize_change_ratio)
+        #print(float(dt))
+        nb_it, converged = solver.solve()
+        if float(dt) < dt_min:
+            sys.exit('Error: stepsize reached minimal value')
+    if t > t_stop:
+        if float(dt) > stepsize_stop_max:
+            dt.assign(stepsize_stop_max)
+    else:
+        if nb_it < 5:
+            dt.assign(float(dt)*stepsize_change_ratio)
+        else:
+            dt.assign(float(dt)/stepsize_change_ratio)
+    
+    if float(dt) > dt_max:
+        dt.assign(dt_max)
+    return dt
+Time = 144*3600
+numstep = 10000000000003*Time
+k = Time/numstep
+dt = Constant(k)
+t = 0.0
 mesh = UnitIntervalMesh(1)
-temp = 300
-parameters["allow_extrapolation"] = True
+temp = 500
+#parameters["allow_extrapolation"] = True
 
-DG0 = FiniteElement('DG', interval, 0)
-element = MixedElement([DG0, DG0])
-V = FunctionSpace(mesh, element)
+
+V = VectorFunctionSpace(mesh, "DG", 0, dim=2)
 
 u = Function(V)
+du = TrialFunction(V)
 cm0, c_surf = split(u)
 
 v0, v_surf = TestFunctions(V)
 u_n = Function(V)
-cm0_n, c_surf_n = split(u_n)
 
+#u_n = interpolate(Expression(('36805096239', '5.5243559532e19'), degree=1), V)
+cm0_n, c_surf_n = split(u_n)
 rho = 6.3e28
 n_surf = 6.9*rho**(2/3)
-print(n_surf)
 n_TIS = 6*rho
 Pr = 0.81
-phi_atom =  5.8e19
+flux_atom = 2.6e19 #Expression("t<50?2.6e19 : (t<98?2.6e19*(1-100/t):1e-2)", t=k, degree=0)
 sigma_exc = 1.7e-21
 k_B = 8.6e-5  # Boltzmann constant
 nu_0d = 1e13
@@ -42,32 +79,48 @@ lambda_abs = n_surf/n_TIS
 E_D = 0.69
 E_A = 1.33
 E_R = 0.2
-grad = -1e17
+grad = Expression("0", t=k, degree=0)#-4.4e23
 alpha = 110e-12
-D = 1e-17
+D = 1.91e-7*np.exp(-0.2/k_B/temp)
 teta = c_surf / n_surf
-phi_atom = (1 - Pr)*phi_atom*(1 - teta)
-phi_exc = phi_atom*sigma_exc*c_surf
-phi_desorb = 2*nu_0d*lambda_des**2*np.exp(-2*E_D/k_B/temp)*c_surf**2
+phi_atom = (1 - Pr)*flux_atom*(1 - teta)
+phi_exc = flux_atom*sigma_exc*c_surf
+phi_desorb = 2*nu_0d*lambda_des**2*np.exp(-2*E_D/k_B/temp)*c_surf**(2)
 phi_sb = nu_0sb*np.exp(-E_A/k_B/temp)*c_surf
 phi_bs = nu_0bs*lambda_abs*np.exp(-E_R/k_B/temp)*cm0*(1-teta)
 phi_diff = -D*grad
 
-F = (cm0 - cm0_n)/dt*v0*dx + (c_surf - c_surf_n)/dt*v_surf*dx
+F = (cm0 - cm0_n)/dt*v0*dx + alpha*(c_surf - c_surf_n)/dt*v_surf*dx
 
-F += -phi_atom*v_surf*dx + phi_exc*v_surf*dx + phi_desorb*v_surf*dx + phi_sb*v_surf*dx - phi_bs*v_surf*dx
+F += -phi_atom*v_surf*dx + phi_exc*v_surf*dx + phi_desorb*v_surf*dx + phi_sb*v_surf*dx - phi_bs*v_surf*dx 
 
-F += 1/alpha * (-phi_sb * v0*dx + phi_bs*v0*dx + phi_diff*v0*dx)
+F += -phi_sb * v0*dx + phi_bs*v0*dx + phi_diff*v0*dx
 
 
 set_log_level(30)
+print(n_surf)
+print(k, u(0.5))
 
+#parameters['allow_extrapolation'] = False
+while t < Time:
 
-parameters['allow_extrapolation'] = False
-for i in range(numstep):
-    
-    solve(F == 0, u, [])
+    J = derivative(F, u, du)  # Define the Jacobian
+
+    problem = NonlinearVariationalProblem(F, u, [], J)
+    solver = NonlinearVariationalSolver(problem)
+    solver.parameters["newton_solver"]["error_on_nonconvergence"] = False
+    #solver.parameters["newton_solver"]['maximum_iterations']=500
+    solver.parameters["newton_solver"]['relative_tolerance'] = 1e-8
+    nb_it, converged = solver.solve()
+    dt = adaptative_timestep(converged=converged, nb_it=nb_it, dt=dt, dt_min=1e-5, dt_max=20,
+                        stepsize_change_ratio=1.1, t=t, t_stop=1000000,
+                        stepsize_stop_max=0.5)
+    t += float(dt)
+    cm0_n, c_surf_n = u_n.split()
+    cm0, c_surf = u.split()
+    print(t, float(dt), cm0(0.5), c_surf(0.5))
     u_n.assign(u)
-    k += dt
-    t.assign(k)
-    print(u(0.5))
+    grad.t += dt
+    #flux_atom.t += dt
+# 36808025644.3 5.52457329233e+19
+# 36807284981.5 5.52451834254e+19
